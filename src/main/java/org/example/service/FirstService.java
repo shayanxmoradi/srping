04aa7d56faf61e034c803f27fe32daf35f7c296e
shayanxmoradi <shayan.moradi92@gmail.com>
2024-09-19 07:58:02 +0200

    xml

---
+++ src/main/java/org/example/service/FirstService.java
@@ -0,0 +1,11 @@
+package org.example.service;
+
+public class FirstService {
+    public FirstService()
+    {
+        System.out.println("first service in contrusctur");
+    }
+    public void afterinit(){
+        System.out.println("first service after init");
+    }
+}
